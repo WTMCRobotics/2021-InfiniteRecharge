f6314c112264e96560f6839c60d96317ac4cd9ed
Nate Stringham <nstringham@wccnet.edu>
2021-02-05 17:36:44 -0500

    fix: VSCode no formatOnSave

--- src/main/java/frc/robot/auton/PresetPath.java
+++ src/main/java/frc/robot/auton/PresetPath.java
@@ -4,7 +4,9 @@ import java.util.List;
 import frc.robot.Robot;
 
 class PresetPath extends Instruction {
-    static final PresetPath AutoNav = new PresetPath(List.of(new MoveInch(1.0)));
+    static final PresetPath AutoNav_BarrelRace = new PresetPath(List.of(new MoveInch(1.0)));
+    static final PresetPath AutoNav_Slalom = new PresetPath(List.of(new MoveInch(1.0)));
+    static final PresetPath AutoNav_Bounce = new PresetPath(List.of(new MoveInch(1.0)));
 
     public List<Instruction> instructions;
     private int currentInstruction = 0;
